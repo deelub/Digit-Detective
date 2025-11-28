f9f6b52ca2503c07fbe83f357a1861c52dcf2249
Divine <Divinelubiku6@gmail.com>
2025-11-28 17:25:41 +0200

    Finished gameplay config, with minor issue of repition in while loop

--- digit_detective.py
+++ digit_detective.py
@@ -1,4 +1,4 @@
-
+import random
 #Additional features : Levels ; Hint systems,Score system(based on how many tries before getting it right) , Attempts tracker, Custom game mode , HARD MODE -CHALLENGE
 def load_game():
   """summary """
@@ -29,7 +29,7 @@ def load_game():
       print("I don't understand that, It seems that you're not quite ready. Type in 'Next' when you're ready")
       play_on=input(" ")
   
-  return "Loading game..."
+  
       
   
   
@@ -37,37 +37,63 @@ def load_game():
 def gameplay_config():
   
   """summary """
-  
-  hints=input(f"Would you like hints during gameplay? (Select a number)\n1.Yes \n2.No \n")
+  global hints_enabled
   hints_enabled= False
+  hint_amt=0
   
-  try:
-    if hints=="1":
-      hints_enabled=True
-      return "yes"
-    elif hints=="2":
-      hints_enabled=False
-      return "no"
-    elif  (int(hints) < 1) or (int(hints) > 2):             
-      while True:
+  while True:
+    try:
+      hints=int(input("Would you like hints during gameplay? (Select a number)\n1.Yes \n2.No \n"))
+      
+      if hints in (1,2):
+        break
+      else:
         print("Invalid option entered,please enter another value")
-        hints= input( " ")
-  except ValueError:
-    while True:
-      print("Invalid character entered,please enter another value")
-      hints= input( " ")
-      
-      
-  hints_amt= 0
-  level=input(f"Would you like hints during gameplay? (Select a number)\n1.Easy \n2.Medium \n3.Hard")
+    except ValueError:
+      print("Invalid option entered,please enter another value")
+
+    #############FIND PLACEHOLDER FOR THIS CODE HERE##########################
+      if hints== 1:   
+        hints_enabled= True
+      elif hints==2:
+        hints_enabled= False
   
-  if level=="1":
-    hints_amt= 6
-  elif level== "2":
-    hints_amt=3
-  elif level== "3":
-    hints_amt=2
+  while True:
+    try:
+      mode=int(input(f"What mode would you like to play? (Select a number)\n1.Easy \n2.Medium \n3.Hard \n"))
+      
+      if mode in (1,3):
+        if mode== 1:
+          hints_amt= 6#see if we should modify the hints amount out
+        elif mode== 2:
+          hints_amt=3
+        elif mode== 3:
+          hints_amt=2
+      else:
+        print("Invalid option, Select a number, 1-3") #Seee the repition on here despite it being rigth after triial and error
+        
+      # return "loading game..."      #find a way to return the game that is being returned
+    except ValueError:
+      print("INvalid option. \nWhat mode would you like to play? (Select a number)\n1.Easy \n2.Medium \n3.Hard \n")
+
+
   
+def easy_mode(hints_enabled): 
+  hint_count= 6
+  
+  if hints_enabled==False:
+    hint_count=0
+  else:   #We can now begin game theory
+    number= random.randrange(1,300)
+
+def medium_mode(hints_enabled):
+  pass
+
+def hard_mode(hints_enabled):
+  pass
+    
+   
+   
   
   
   
@@ -77,7 +103,8 @@ def play_game():
 
   load_game()
   gameplay_config()
+  
 
 
 
-print(gameplay_config())
+print(play_game())
